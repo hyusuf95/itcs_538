4a698d669dc7c4e5971e9bc1136136fafa51d059
Hussain Yusuf <hussain.yusuf95@gmail.com>
2023-07-14 20:11:05 +0300

    Training data done but not prepared data

--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
 from Housing import Housing
-
+from pandas.core.frame import DataFrame
 
 house: Housing = Housing('data.csv')
 
-print(house.outlier_analysis())
+
+
+prepared_data = house.prepare_data()
+
+
+trained_model: DataFrame = house.tain_data()
+#print the type of trained_model
+(trained_model.describe())
